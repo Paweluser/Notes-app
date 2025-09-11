30aad0c55c3ed3a4a46f4307c7bc8cf47af9804b
Pawel <ppazyrski@wp.pl>
2025-09-11 22:54:27 +0200

    feat: add glass panel for folders and notes, animate menu button

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,17 +1,20 @@
+import AddButton from '@/components/AddButton';
 import Menu from '@/components/Menu';
 import SearchEngine from '@/components/SearchEngine';
 
 export default function NotesPage() {
 	return (
-		<div className="flex flex-col h-full w-full p-4">
-			<nav className="flex justify-between px-2 xl:justify-around mt-3 lg:mt-5 xl:mt-14">
+		<div className="flex flex-col h-full p-4">
+			<nav className="flex justify-between px-2 mb-3 xl:justify-around mt-3 lg:mt-5 xl:mt-14">
 				<h2 className="text-center bg-linear-65 from-purple-400 to-pink-200 bg-clip-text [-webkit-background-clip:text] text-transparent text-4xl md:text-5xl">
 					Notes
 				</h2>
 				<Menu />
 			</nav>
 			<SearchEngine />
-			<div className="flex flex-col p-3 bg-[var(--dark-color)] w-full mt-3 xl:w-2/3 xl:mx-auto xl:mt-6"></div>
+			<div className="relative flex gap-2 w-full h-2/3 mt-3 xl:w-2/3 xl:mx-auto xl:mt-6 bg-[var(--second-color)]/20 backdrop-blur rounded-2xl shadow-2xl">
+				<AddButton />
+			</div>
 		</div>
 	);
 }
---
+++ src/components/AddButton.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function AddButton() {
+	return (
+		<button className="absolute right-3 text-[var(--third-color)] cursor-pointer text-5xl py-2 px-2 h-fit w-fit">
+			+
+		</button>
+	);
+}
