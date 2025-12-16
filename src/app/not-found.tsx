de2504845226de21efc92d866ad5c8849c4d6ee4
Pawel <ppazyrski@wp.pl>
2025-12-16 21:08:01 +0100

    Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import FormButtonPage from '@/components/FormButtonPage';
+
+export default function NotFound() {
+	return (
+		<div className="relative flex flex-col justify-center items-center text-xl md:text-2xl p-3 h-2/3">
+			<p className="text-gradient">Oops! This page doesn't exist!</p>
+			<div className='mt-5 md:mt-8'>
+				<FormButtonPage href="/">Back</FormButtonPage>
+			</div>
+		</div>
+	);
+}
